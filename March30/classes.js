effff270cb90642cb5ef02a04ba247e066a86b85
harmonyrhi <37599401+harmonyrhi@users.noreply.github.com>
2022-04-01 13:27:42 -0400

    Submitting Classes Assignment

--- March30/classes.js
+++ March30/classes.js
@@ -1,7 +1,7 @@
 const { is } = require("express/lib/request");
 
 class Hero{
-    constructor(name, alias, location, nemesis, friends, power){
+    constructor(name, alias, location, nemesis, friends){
         this.name = name;
         this.alias = alias;
         this.location = location;
@@ -9,23 +9,35 @@ class Hero{
         this.friends = [friends];
         this.isAlive = true;
         this.powerLevel = 100;
-        this.power = {
-            power1 : 10,
-            power2 : 5,
-            power3 : 15,
-            power4 : 20
-        };
+        this.hasPowers = false;
+        // this.powers = powers;
+        // this.powerAmt = powerAmt;
+        // {
+        //     power1 : 5,
+        //     power2 : 10,
+        //     power3 : 15,
+        //     power4 : 20
+        // };
     };
-        usePower(){
-            if(this.isAlive){
-            this.powerAmt > this.powerLevel ? console.log("warning: hero does not have enough energy to use this power") : this.powerLevel -= this.powerAmt;            
-            };
+        givePowers(power, powerAmt){
+            ({ power : powerAmt } = Hero);
+            this.hasPowers = true;
+        }
+
+        usePower(power){
+            if(this.isAlive && this.hasPowers){
+                power > this.powerLevel ? console.log("warning: hero does not have enough energy to use this power") : this.powerLevel -= power;            
+            } else if (this.isAlive){
+                console.log('warning: this hero does not have any powers to use');
+            }
+
         };
 
         forcePower(){
             this.powerLevel -= this.powerAmt;
             if (this.powerLevel < 0) {
-               this.isAlive = false; 
+               this.isAlive = false;
+               this.hasPowers = false; 
             };
         };
 
@@ -64,43 +76,29 @@ class Superhero extends Hero {
 
 }
 
-let harley = new Hero("Dr. Harleen Quinzel", "Harley Quinn", "Gotham", "Joker", "Poison Ivy", "Being underestimated");
+let harley = new Hero("Dr. Harleen Quinzel", "Harley Quinn", "Gotham", "Joker", "Poison Ivy");
 console.log(harley);
-harley.usePower();
-harley.addFriend('harmony');
-harley.addNemesis("responsibility");
-console.log(harley);
-harley.usePower();
-harley.usePower();
-harley.usePower();
-harley.usePower();
-console.log(harley);
-harley.usePower();
-harley.usePower();
-harley.usePower();
-harley.usePower();
-harley.usePower();
-harley.usePower();
+harley.givePowers("bat", 10);
+// harley.usePower("bat");
+// harley.addFriend('harmony');
+// harley.addNemesis("responsibility");
+// console.log(harley);
+// harley.usePower();
+// harley.usePower();
+// harley.usePower();
+// harley.usePower();
+// console.log(harley);
+// harley.usePower();
+// harley.usePower();
+// harley.usePower();
+// harley.usePower();
+// harley.usePower();
+// harley.usePower();
 
 console.log(harley);
 
 let ivy = new Superhero("Dr. Pamela Isley", "Poison Ivy", "Gotham", "Global Warming","Harley Quinn", "Plant manipulation", 10);
 console.log(ivy);
-ivy.usePower();
-ivy.addNemesis("Joker")
-console.log(ivy);
-
-// const names =["harmony", "harmony1", "harmony3", "harmony4", "harmony5"];
-
-// const [firstName, ,secondName] = names;
-
-// console.log(secondName);
-
-// const player = {
-//     name : "harmony", 
-//     age : 24,
-//     location : 'houston',
-//     sport: "baseball"
-// };
-// let {name, sport} = player;
-// console.log(name);
+// ivy.usePower();
+// ivy.addNemesis("Joker")
+// console.log(ivy);
